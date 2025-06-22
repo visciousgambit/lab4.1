3c93fa31f19df60c56d381a06f7df365217b63f4
Daniel Ward <kingsgambit35@gmail.com>
2025-06-22 00:52:34 -0400

    feature/added

--- lab4.1.js
+++ lab4.1.js
@@ -1,10 +1,11 @@
- 
 
-let firstName = "John";
 
-let lastName = "Doe";
+function formatFullName() {
 
-function formatFullName () {
+    let firstName = "John";
+
+    let lastName = "Doe";
+
     console.log(lastName)
     console.log(firstName)
 }
@@ -14,7 +15,17 @@ formatFullName();
 
 
 
-function calculateTotalCost () {
-    
+function calculateTotalCost() {
+    let price = "2"
+    let quantity = "4";
+    let taxRate = "0.4";
+    let discount = ".1"
+    calculateTotalCost = (price * quantity) * (1 + taxRate)*(discount)
+
 }
+
+calculateTotalCost();
+
+console.log(calculateTotalCost);
+
 
